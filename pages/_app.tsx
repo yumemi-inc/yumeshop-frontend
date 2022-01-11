ff763033876bfb1e28fb9edc2e4678be0007b11a
yumemi-kawasumi <a_kawasumi@yumemi.co.jp>
2022-01-11 19:33:25 +0900

    fix: styles to styled components from global

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
+import { GlobalStyle } from '../style/Globals'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle/>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
