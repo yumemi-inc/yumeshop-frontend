edf0b4cf03f9be0fccc06532abcc3ed6a9ffc268
yumemi-kawasumi <a_kawasumi@yumemi.co.jp>
2022-01-11 19:52:06 +0900

    fix: add reset style

--- style/Globals.ts
+++ style/Globals.ts
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from 'styled-components'
+import { reset } from 'styled-reset'
 
 export const GlobalStyle = createGlobalStyle`
+  ${reset};
+
   html,
   body {
     padding: 0;
