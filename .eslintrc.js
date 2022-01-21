2df658ace8feb67965a674e5bcedf7ccc9bfcc66
yumemi-omi <hiro.041310402.kt@gmail.com>
2022-01-21 14:54:11 +0900

    fix: change the way test files are specified about eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,8 +30,8 @@ module.exports = ({
     'import/no-extraneous-dependencies': [
       'error', {
         devDependencies: [
-          '**/*.test.{js,jsx,ts,tsx}',
-          '**/*.spec.{js,jsx,ts,ts}',
+          '**/__tests__/**/*.[jt]s?(x)',
+          '**/?(*.)+(spec|test).[jt]s?(x)',
           '**/jest.setup.js'
         ]
       }
