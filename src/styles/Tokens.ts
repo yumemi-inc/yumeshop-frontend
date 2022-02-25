eac0ea989500116fb23a430c63502a46d814b7c7
Marina Ivanova <m_ivanova@yumemi.co.jp>
2022-02-25 17:12:42 +0900

    margin, font sizes追加

--- src/styles/Tokens.ts
+++ src/styles/Tokens.ts
@@ -7,3 +7,22 @@ export const colors = {
   Gray: '#C4C4C4',
   Black: '#000000',
 } as const;
+
+export const spacingSizes = {
+  auto: 'auto',
+  none: 'none',
+  xxxs: '0.125rem', // 2px
+  xxs: '0.25rem', // 4px
+  xs: '0.5rem', // 8px
+  sm: '1rem', // 16px
+  md: '2rem', // 32px
+  lg: '4rem', // 64px
+  xl: '8rem', // 128px
+  xxl: '16rem', // 256px
+} as const;
+
+export const fontSizes = {
+  small: '0.75rem', //12px
+  medium: '0.875rem', //14px
+  large: '1rem', //16px
+} as const;
