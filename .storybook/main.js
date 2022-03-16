5e45f67df231d5d4fdb17368db63595e7ea4153b
Takamasa Suzuki <midgard10257@gmail.com>
2022-03-16 14:21:19 +0900

    fix: `yarn format` コマンド実行した自動修正分の反映

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,10 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   webpackFinal: async (baseConfig) => {
-    baseConfig.resolve.modules = [...(baseConfig.resolve.modules || []), path.resolve(__dirname, '../')];
+    baseConfig.resolve.modules = [
+      ...(baseConfig.resolve.modules || []),
+      path.resolve(__dirname, '../'),
+    ];
     return baseConfig;
   },
 };
