243b7a2f9b08de0c11a514b42197510dc293dcc8
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-05-11 16:09:16 +0900

    feat: PicAndTextCardコンポーネントの中身を実装

--- src/components/molecules/PicAndTextCard/index.tsx
+++ src/components/molecules/PicAndTextCard/index.tsx
@@ -1,10 +1,26 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Image, Text, BoxProps } from '@chakra-ui/react';
 import React, { VFC } from 'react';
 
-interface PicAndTextCardProps {
-  key: string;
+interface PicAndTextCardProps extends BoxProps {
   title: string;
   thumbnailUrl: string;
 }
 
-export const PicAndTextCard: VFC<PicAndTextCardProps> = () => <Box />;
+/**
+ * 画像とテキストを含むカード
+ * @param title
+ * @param thumbnailUrl
+ * @param width
+ */
+export const PicAndTextCard: VFC<PicAndTextCardProps> = ({
+  title,
+  thumbnailUrl,
+  width,
+}) => (
+  <Box borderRadius={8} shadow="base" overflow="hidden" width={width}>
+    <Image src={thumbnailUrl} alt={title} />
+    <Box p={2}>
+      <Text fontSize="sm">{title}</Text>
+    </Box>
+  </Box>
+);
