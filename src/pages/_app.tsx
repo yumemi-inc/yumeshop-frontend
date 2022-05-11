8c9f3c5f82147c70f331a1c204ec386059a07b70
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-05-11 14:40:19 +0900

    feat: _app.tsxにChakraProviderを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import type { AppProps } from 'next/app';
 import GlobalStyle from 'src/styles/Globals';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ChakraProvider>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </ChakraProvider>
   );
 }
 
