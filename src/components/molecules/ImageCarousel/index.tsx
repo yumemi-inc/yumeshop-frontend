980eef0daf0347ae1c994832d3d47a2f8e63d76e
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-05-12 15:50:31 +0900

    feat: レスポンシブルにカルーセル表示が変わるように実装

--- src/components/molecules/ImageCarousel/index.tsx
+++ src/components/molecules/ImageCarousel/index.tsx
@@ -17,21 +17,39 @@ interface ImageCarouselProps extends BoxProps {
   items: Item[];
 }
 
-const setting: Settings = {
+// breakpointは768以下
+const commonSetting: Settings = {
   dots: true,
   infinite: true,
   centerMode: true,
   slidesToShow: 1,
-  centerPadding: '25%',
+  arrows: false,
+  centerPadding: '0px',
+  variableWidth: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        variableWidth: false,
+      },
+    },
+  ],
 };
 
 export const ImageCarousel: VFC<ImageCarouselProps> = ({ items, ...props }) => (
-  //
   <Box {...props}>
-    <Slick {...setting}>
+    <Slick {...commonSetting}>
       {items.map((item) => (
-        <Box key={item.key} px={4}>
+        <Box
+          key={item.key}
+          px={{
+            base: 0,
+            md: 4,
+          }}
+          maxWidth="600px"
+        >
           <NextLink href={item.href} passHref>
+            {/* eslintではhref Propの無いリンク系タグにwarnが出るが、ここではNextLinkから渡されるのでdisable */}
             {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
             <Link>
               <Center>
