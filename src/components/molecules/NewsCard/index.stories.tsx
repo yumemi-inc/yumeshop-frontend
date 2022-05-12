7bde806764399f96630bf4c777426637db825f07
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-05-12 10:46:56 +0900

    feat: date表示を除くコンポーネントの中身を実装

--- src/components/molecules/NewsCard/index.stories.tsx
+++ src/components/molecules/NewsCard/index.stories.tsx
@@ -11,4 +11,20 @@ type NewsCardProps = React.ComponentProps<typeof NewsCard>;
 const Template: Story<NewsCardProps> = (args) => <NewsCard {...args} />;
 
 export const General = Template.bind({});
-General.args = {};
+General.args = {
+  title: 'メンテナンスのお知らせ',
+  date: new Date(2022, 4, 11),
+  href: '#',
+  tags: [
+    {
+      key: '57c3ff77-d8bd-41bb-86e3-4526e1b2186c',
+      name: '重要',
+      color: 'red',
+    },
+    {
+      key: '57c3ff77-d8bd-41bb-86e3-4526e1b2186d',
+      name: 'メンテナンス',
+      color: 'yellow',
+    },
+  ],
+};
--- src/components/molecules/NewsCard/index.tsx
+++ src/components/molecules/NewsCard/index.tsx
@@ -1,6 +1,47 @@
-import { Box } from '@chakra-ui/react';
+import {
+  BoxProps,
+  Heading,
+  HStack,
+  Stack,
+  Text,
+  Tag,
+  LinkBox,
+  LinkOverlay,
+} from '@chakra-ui/react';
 import React, { VFC } from 'react';
+import NextLink from 'next/link';
 
-interface NewsCard {}
+interface Tag {
+  key: string;
+  name: string;
+  color: string;
+}
 
-export const NewsCard: VFC<NewsCard> = (props) => <Box>{/*  */}</Box>;
+interface NewsCard extends BoxProps {
+  title: string;
+  date: Date;
+  tags: Tag[];
+  href: string;
+}
+
+export const NewsCard: VFC<NewsCard> = ({ title, tags, href, ...props }) => (
+  <LinkBox borderRadius={8} shadow="base" p={2} {...props}>
+    <Stack>
+      <HStack>
+        <Heading size="sm">2022/05/11</Heading>
+        <HStack>
+          {tags.map((tag) => (
+            <Tag key={tag.key} backgroundColor={tag.color} borderRadius="none">
+              {tag.name}
+            </Tag>
+          ))}
+        </HStack>
+      </HStack>
+      <NextLink href={href} passHref>
+        <LinkOverlay>
+          <Text noOfLines={2}>{title}</Text>
+        </LinkOverlay>
+      </NextLink>
+    </Stack>
+  </LinkBox>
+);
