cff8e9b3972525f729762006f8b21a6b8dddeee4
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-05-12 11:17:28 +0900

    refactor: storyの書き方をCSF3.0に変更

--- src/components/molecules/NewsCard/index.stories.tsx
+++ src/components/molecules/NewsCard/index.stories.tsx
@@ -1,30 +1,27 @@
 import { NewsCard } from 'src/components/molecules/NewsCard/index';
-import { Meta, Story } from '@storybook/react/types-6-0';
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
 
 export default {
   component: NewsCard,
   title: 'molecules/NewsCard',
-} as Meta;
+} as ComponentMeta<typeof NewsCard>;
 
-type NewsCardProps = React.ComponentProps<typeof NewsCard>;
-
-const Template: Story<NewsCardProps> = (args) => <NewsCard {...args} />;
-
-export const General = Template.bind({});
-General.args = {
-  title: 'メンテナンスのお知らせ',
-  date: new Date(2020, 12 - 1, 23),
-  href: '#',
-  tags: [
-    {
-      key: '57c3ff77-d8bd-41bb-86e3-4526e1b2186c',
-      name: '重要',
-      color: '#E20E20',
-    },
-    {
-      key: '57c3ff77-d8bd-41bb-86e3-4526e1b2186d',
-      name: 'メンテナンス',
-      color: '#E89244',
-    },
-  ],
+export const General: ComponentStoryObj<typeof NewsCard> = {
+  args: {
+    title: 'メンテナンスのお知らせ',
+    date: new Date(2020, 12 - 1, 23),
+    href: '#',
+    tags: [
+      {
+        key: '57c3ff77-d8bd-41bb-86e3-4526e1b2186c',
+        name: '重要',
+        color: '#E20E20',
+      },
+      {
+        key: '57c3ff77-d8bd-41bb-86e3-4526e1b2186d',
+        name: 'メンテナンス',
+        color: '#E89244',
+      },
+    ],
+  },
 };
