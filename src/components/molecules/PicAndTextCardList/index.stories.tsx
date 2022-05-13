f263a9fef5ce3fcfaed659626ade26dc2b05cb03
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-05-13 12:06:19 +0900

    feat: NewsCardListのストーリーデータの種類を増やした

--- src/components/molecules/PicAndTextCardList/index.stories.tsx
+++ src/components/molecules/PicAndTextCardList/index.stories.tsx
@@ -1,5 +1,5 @@
 import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
-import { PicAndTextCardList } from 'src/components/molecules/PicAndTextCardList/index';
+import { PicAndTextCardList } from './index';
 
 export default {
   component: PicAndTextCardList,
