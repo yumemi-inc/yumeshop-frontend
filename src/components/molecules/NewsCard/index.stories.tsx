1a6110d1a7b43ec3c9211dc0b3dfdbe70052c6ad
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-05-13 12:02:29 +0900

    fix: 型定義変更に伴いstoryが壊れていたので修正

--- src/components/molecules/NewsCard/index.stories.tsx
+++ src/components/molecules/NewsCard/index.stories.tsx
@@ -6,21 +6,30 @@ export default {
   title: 'molecules/NewsCard',
 } as ComponentMeta<typeof NewsCard>;
 
+const tags = {
+  important: {
+    key: 'd7b071eb-37f6-4fb7-be6f-ac7a33a03761',
+    name: '重要',
+    backGroundColor: 'red',
+  },
+  maintenance: {
+    key: '4ca12003-6c27-4ac0-8c76-5b41662a8077',
+    name: 'メンテナンス',
+    backGroundColor: 'mediumpurple',
+  },
+  newProduct: {
+    key: '84e4e574-073c-4e00-8d18-c2c8ea4b7421',
+    name: '新商品',
+    backGroundColor: 'orange',
+  },
+} as const;
+
 export const General: ComponentStoryObj<typeof NewsCard> = {
   args: {
     title: 'メンテナンスのお知らせ',
     date: new Date('2017-07-21T17:32:28Z'),
     href: '#',
-    tags: [
-      {
-        name: '重要',
-        backGroundColor: 'red',
-      },
-      {
-        name: 'メンテナンス',
-        backGroundColor: 'yellow',
-      },
-    ],
+    tags: [tags.important, tags.maintenance],
   },
 };
 
@@ -28,15 +37,6 @@ export const WithoutLink: ComponentStoryObj<typeof NewsCard> = {
   args: {
     title: 'メンテナンスのお知らせ',
     date: new Date('2017-07-21T17:32:28Z'),
-    tags: [
-      {
-        name: '重要',
-        backGroundColor: 'red',
-      },
-      {
-        name: 'メンテナンス',
-        backGroundColor: 'yellow',
-      },
-    ],
+    tags: [tags.important, tags.maintenance],
   },
 };
--- src/components/molecules/NewsCardList/index.stories.tsx
+++ src/components/molecules/NewsCardList/index.stories.tsx
@@ -1,10 +1,28 @@
-import { NewsCardList } from 'src/components/molecules/NewsCardList/index';
 import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
+import { NewsCardList } from './index';
 
 export default {
   component: NewsCardList,
   title: 'molecules/NewsCardList',
 } as ComponentMeta<typeof NewsCardList>;
+
+const tags = {
+  important: {
+    key: 'd7b071eb-37f6-4fb7-be6f-ac7a33a03761',
+    name: '重要',
+    backGroundColor: 'red',
+  },
+  maintenance: {
+    key: '4ca12003-6c27-4ac0-8c76-5b41662a8077',
+    name: 'メンテナンス',
+    backGroundColor: 'mediumpurple',
+  },
+  newProduct: {
+    key: '84e4e574-073c-4e00-8d18-c2c8ea4b7421',
+    name: '新商品',
+    backGroundColor: 'orange',
+  },
+} as const;
 
 export const General: ComponentStoryObj<typeof NewsCardList> = {
   args: {
@@ -14,48 +32,21 @@ export const General: ComponentStoryObj<typeof NewsCardList> = {
         title: 'メンテナンスのお知らせ',
         date: new Date('2017-07-21T17:32:28Z'),
         href: '#',
-        tags: [
-          {
-            name: '重要',
-            backGroundColor: 'red',
-          },
-          {
-            name: 'メンテナンス',
-            backGroundColor: 'yellow',
-          },
-        ],
+        tags: [tags.important, tags.maintenance],
       },
       {
         key: '57c3ff77-d8bd-41bb-86e3-4526e1b2186e',
         title: 'メンテナンスのお知らせ',
         date: new Date('2017-07-21T17:32:28Z'),
         href: '#',
-        tags: [
-          {
-            name: '重要',
-            backGroundColor: 'red',
-          },
-          {
-            name: 'メンテナンス',
-            backGroundColor: 'yellow',
-          },
-        ],
+        tags: [tags.important, tags.maintenance],
       },
       {
         key: '57c3ff77-d8bd-41bb-86e3-4526e1b2186d',
         title: 'メンテナンスのお知らせ',
         date: new Date('2017-07-21T17:32:28Z'),
         href: '#',
-        tags: [
-          {
-            name: '重要',
-            backGroundColor: 'red',
-          },
-          {
-            name: 'メンテナンス',
-            backGroundColor: 'yellow',
-          },
-        ],
+        tags: [tags.important, tags.maintenance],
       },
     ],
   },
