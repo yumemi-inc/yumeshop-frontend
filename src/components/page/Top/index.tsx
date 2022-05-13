67a7e01b4836d3aaaf7dbfc23eecd7c6e5a662df
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-05-13 15:27:25 +0900

    feat: カルーセルの配置まで

--- src/components/page/Top/index.tsx
+++ src/components/page/Top/index.tsx
@@ -1,4 +1,16 @@
-import { VFC } from 'react';
+import { Suspense, VFC } from 'react';
 import { Box } from '@chakra-ui/react';
+import { usePromotionCardsQuery } from 'src/libs/hooks/usePromotionCardsQuery';
+import { ImageCarousel } from 'src/components/molecules/ImageCarousel';
 
-export const TopPage: VFC = () => <Box />;
+export const TopPage: VFC = () => {
+  const promotionData = usePromotionCardsQuery();
+
+  return (
+    <Box>
+      <Suspense fallback={<p>Loading...</p>}>
+        <ImageCarousel items={promotionData} />
+      </Suspense>
+    </Box>
+  );
+};
