15d0aa8f2e565e26f9a1805d1801371c85f25fac
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-05-13 16:43:24 +0900

    fix: chakra-ui v2ではToastContainerを配置する必要がある

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import type { AppProps } from 'next/app';
 import defaultChakraTheme from 'src/styles/Globals';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import {
+  ChakraProvider,
+  createStandaloneToast,
+  extendTheme,
+} from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 const theme = extendTheme(defaultChakraTheme);
@@ -13,11 +17,14 @@ const queryClient = new QueryClient({
   },
 });
 
+const { ToastContainer } = createStandaloneToast();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme}>
         <Component {...pageProps} />
+        <ToastContainer />
       </ChakraProvider>
     </QueryClientProvider>
   );
