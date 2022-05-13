7dfe47aaf9f2e48ab768f267998ad18e26d70295
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-05-13 10:44:15 +0900

    feat: _app.tsxにreact-queryのproviderを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,25 @@
 import type { AppProps } from 'next/app';
 import defaultChakraTheme from 'src/styles/Globals';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const theme = extendTheme(defaultChakraTheme);
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
 
