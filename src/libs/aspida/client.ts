5fdd72bd3a1e184fa8e22fc6ac4c2b2e9f771c98
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-05-13 13:52:32 +0900

    fix: aspidaClientのbaseURL指定が抜けていたので環境変数から参照するように

--- src/libs/aspida/client.ts
+++ src/libs/aspida/client.ts
@@ -1,4 +1,8 @@
 import api from 'src/libs/aspida/api/$api';
 import aspida from '@aspida/fetch';
 
-export const client = api(aspida());
+export const client = api(
+  aspida(fetch, {
+    baseURL: process.env.NEXT_PUBLIC_API_ROOT,
+  }),
+);
