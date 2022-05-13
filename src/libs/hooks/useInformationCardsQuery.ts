7873779b6bb2b7d3b2ff6d9a65d5e79dc44f3770
elecdeer <37701077+elecdeer@users.noreply.github.com>
2022-05-13 11:39:37 +0900

    feat: useInformationCardsQueryを実装

---
+++ src/libs/hooks/useInformationCardsQuery.ts
@@ -0,0 +1,25 @@
+import { useAspidaQuery } from '@aspida/react-query';
+import { useMemo } from 'react';
+import assert from 'assert';
+import { NewsCardListItem } from 'src/components/molecules/NewsCardList';
+import { client } from '../aspida/client';
+
+export const useInformationCardsQuery = (href?: string) => {
+  const { data } = useAspidaQuery(client.informations);
+  assert(data); // suspenseなのでundefinedにはならない
+  return useMemo<NewsCardListItem[]>(
+    () =>
+      data.map((item) => ({
+        key: item.id,
+        title: item.title,
+        tags: item.tags.map((tagItem) => ({
+          key: tagItem.id,
+          name: tagItem.name,
+          backGroundColor: tagItem.color,
+        })),
+        date: new Date(item.announced_at),
+        href,
+      })),
+    [data, href],
+  );
+};
