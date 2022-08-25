a746d42655f674a6b716260096299aeeb451c961
tosssssy <t.kabu.03@gmail.com>
2022-08-25 11:18:45 +0900

    feat: SWRを使用してカスタムフック作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { useState } from 'react';
+import { useGetApi } from 'src/hooks/useGetApi';
 import { colors, fontSizes } from 'src/styles/Tokens';
-import { ShopItemDetail } from 'src/types/shopItem';
-import { getApi, ApiResponseError } from 'src/utils/api';
 
 const Home: NextPage = () => {
-  const [data, setData] = useState<ShopItemDetail>();
+  const { data } = useGetApi('/shop_items/:id', {
+    id: '4dfd9672-5633-4328-b104-832e2f18c2a7',
+  });
 
   return (
     <div>
@@ -24,25 +24,6 @@ const Home: NextPage = () => {
       >
         テスト
       </div>
-      <button
-        type="button"
-        onClick={async () => {
-          try {
-            // const res = await getApi("/shop_items")
-            const res = await getApi('/shop_items/:id', {
-              id: '4dfd9672-5633-4328-b104-832e2f18c2a7',
-            });
-            setData(res);
-          } catch (error) {
-            if (error instanceof ApiResponseError) {
-              // eslint-disable-next-line no-console
-              console.error(error);
-            }
-          }
-        }}
-      >
-        fetch
-      </button>
 
       {JSON.stringify(data)}
     </div>
