5c09c405109a7dd4f28045b2c8b79fe787f79a23
tosssssy <t.kabu.03@gmail.com>
2022-08-25 10:29:38 +0900

    fix: 配列の型定義を修正

--- src/types/shopItem.ts
+++ src/types/shopItem.ts
@@ -11,37 +11,30 @@ export type ShopItem = {
   name: string;
   thumbnail: string;
   price: Price;
-  tags: [
-    {
-      id: string;
-      name: string;
-      color: string;
-      tag_group: string;
-    },
-  ];
-  campaigns: [
-    {
-      id: string;
-      name: string;
-      thumbnail: string;
-    },
-  ];
-  categories: [
-    {
-      id: string;
-      name: string;
-      thumbnail: string;
-    },
-  ];
+  tags: {
+    id: string;
+    name: string;
+    color: string;
+    tag_group: string;
+  }[];
+  campaigns: {
+    id: string;
+    name: string;
+    thumbnail: string;
+  }[];
+
+  categories: {
+    id: string;
+    name: string;
+    thumbnail: string;
+  }[];
 };
 
 export type ShopItemDetail = ShopItem & {
-  details: [
-    {
-      header: string;
-      content: string;
-    },
-  ];
+  details: {
+    header: string;
+    content: string;
+  }[];
   images: string[];
   related_shop_items: ShopItem[];
 };
