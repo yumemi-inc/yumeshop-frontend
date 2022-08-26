fcf0b5868ac18aec3360c87833d52813c6565cdf
tosssssy <t.kabu.03@gmail.com>
2022-08-26 11:59:08 +0900

    fix: コンポーネントを制御しやすい設計にするためcss propは受け取らない方針に変更

--- src/components/common/SectionTitle/SectionTitle.tsx
+++ src/components/common/SectionTitle/SectionTitle.tsx
@@ -1,19 +1,16 @@
-import { SerializedStyles } from '@emotion/react';
 import { FC } from 'react';
 import { fontSizes } from 'src/styles/Tokens';
 
 export type SectionTitleProps = {
   children: string;
-  css?: SerializedStyles;
 };
 
-export const SectionTitle: FC<SectionTitleProps> = ({ children, css }) => (
+export const SectionTitle: FC<SectionTitleProps> = ({ children }) => (
   <h2
     css={{
       fontWeight: 700,
       fontSize: fontSizes.fontSize24,
       lineHeight: '35px',
-      ...css,
     }}
   >
     {children}
