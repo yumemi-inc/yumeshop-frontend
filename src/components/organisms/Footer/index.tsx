d46f797dd4d8b5455b4c62d352e4059d611f4b77
熊倉 秀人 <h_kumakura@MacBook-Pro.local>
2022-09-06 13:39:49 +0900

    add menu to footer

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -2,18 +2,59 @@ import { VFC } from 'react';
 import styled from 'styled-components';
 import { colors } from 'src/styles/Tokens';
 
+
 const Wrapper = styled.footer`
   width: 100%;
   padding: 16px 0;
   font-size: 12px;
   background-color: ${colors.White};
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  ul {
+    justify-content: start;
+    column-count: 3;
+    display: flex;
+    list-style: none;
+  }
+  li {
+    padding-right: 4px;
+    padding-left: 4px;
+  }
+  li + li {
+    list-style: "|";
+  }
+  @media screen and (width >= 1152px) {
+    li {
+      padding-right: 8px;
+      padding-left: 8px;
+    }
+  }
+`;
+  
+const Copyright = styled.p`
+  margin-top: 64px;
+  text-align: center;
+  @media screen and (width >= 1152px) {
+    margin-top: 32px;
+  }
 `;
 
 export const Footer: VFC = () => (
   <Wrapper>
-    <p>Copyright {new Date().getFullYear()} YumemiShop</p>
+    <ul>
+      <li>メニューメニュー</li>
+      <li>メニューメニュー</li>
+      <li>メニュー</li>
+    </ul>
+    <ul>
+      <li>メニューメニュー</li>
+      <li>メニュー</li>
+      <li>メニュー</li>
+      <li>メニュー</li>
+    </ul>
+    <ul>
+      <li>メニューメニュー</li>
+      <li>メニューメニュー</li>
+      <li>メニューメニュー</li>
+    </ul>
+    <Copyright>Copyright {new Date().getFullYear()} YumemiShop</Copyright>
   </Wrapper>
 );
