47176c640dd145497741d8b8adf86b7de509da44
梅原 隆一 <r_umehara@MacBook-Pro.local>
2022-10-24 16:09:50 +0900

    fix: delete style component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,9 @@
 import type { AppProps } from 'next/app';
-import GlobalStyle from 'src/styles/Globals';
 import { ChakraProvider } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <GlobalStyle />
       <Component {...pageProps} />
     </ChakraProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,10 @@
+import { Box } from '@chakra-ui/react';
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-
-import { Container, Main, Title, TokenTest } from 'src/styles/Home';
 
 const Home: NextPage = () => (
-  <Container>
-    <Head>
-      <title>Yumeshop</title>
-      <meta name="description" content="" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-
-    <Main>
-      <Image src="/shopping-bag.jpg" alt="買い物袋" width={600} height={600} />
-
-      <TokenTest>
-        <Title>Welcome to Yumeshop</Title>
-      </TokenTest>
-    </Main>
-  </Container>
+  <Box width={100} height={100} bg="black">
+    <p color="black">homepage</p>
+  </Box>
 );
 
 export default Home;
