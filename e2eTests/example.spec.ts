83d3df35fdf5f68b9f5097ad19f68d583f4a34a4
梅原 隆一 <r_umehara@MacBook-Pro.local>
2022-10-25 14:36:40 +0900

    fix: add timeout

--- e2eTests/example.spec.ts
+++ e2eTests/example.spec.ts
@@ -3,6 +3,7 @@ import { test, expect, type Page } from '@playwright/test';
 test.describe('', () => {
   test('can go to campaign page', async ({ page }) => {
     await page.goto('/', { waitUntil: 'domcontentloaded' });
+    await page.waitForTimeout(1000);
     await page.screenshot({ path: 'top-page.png' });
     await page.locator('data-testid=campaign-card-1').first().click();
     await expect(page).toHaveURL('http://localhost:3000/campaign');
