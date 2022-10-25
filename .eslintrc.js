84ffb474d98e841d33dc16fcffb34f17e88814ca
梅原 隆一 <r_umehara@MacBook-Pro.local>
2022-10-25 12:25:03 +0900

    fix: path

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   ],
   rules: {
     'react/react-in-jsx-scope': 'off',
+    "jsx-a11y/anchor-is-valid": [ "off"],
     'import/extensions': [
       'error',
       'ignorePackages',
