b6fbb3fb806a9b0fb0e13f247e25e4caac1007b0
Hiroki Iwakubo <emrum01@gmail.com>
2023-05-19 07:35:27 +0900

    fix: mdxをstoriesプロパティから削除
    存在しない場合警告が出るので

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,19 @@
 const path = require('path');
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  webpackFinal: async baseConfig => {
-    baseConfig.resolve.modules = [...(baseConfig.resolve.modules || []), path.resolve(__dirname, '../')];
+  webpackFinal: async (baseConfig) => {
+    baseConfig.resolve.modules = [
+      ...(baseConfig.resolve.modules || []),
+      path.resolve(__dirname, '../'),
+    ];
     return baseConfig;
   },
   framework: {
     name: '@storybook/nextjs',
-    options: {}
+    options: {},
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
