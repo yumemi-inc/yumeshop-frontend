c73e0d7b3f9e7963a9c1c73561378a20faf27c6f
Hiroki Iwakubo <emrum01@gmail.com>
2023-05-19 07:16:34 +0900

    chore: storybookをupgradeした
    npx storybook@latest upgrade

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,16 @@
 const path = require('path');
-
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  webpackFinal: async (baseConfig) => {
-    baseConfig.resolve.modules = [
-      ...(baseConfig.resolve.modules || []),
-      path.resolve(__dirname, '../'),
-    ];
+  webpackFinal: async baseConfig => {
+    baseConfig.resolve.modules = [...(baseConfig.resolve.modules || []), path.resolve(__dirname, '../')];
     return baseConfig;
   },
-  core: {
-    builder: 'webpack5',
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
   },
+  docs: {
+    autodocs: true
+  }
 };
