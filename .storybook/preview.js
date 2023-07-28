6da99331fffb94fb655b34b84f6c213e0694ddca
Kei Ota <k_ota@yumemi.co.jp>
2023-07-28 15:38:29 +0900

    動くように preview.js を修正

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,24 +1,11 @@
 // .storybook/preview.js
 import * as NextImage from 'next/image';
-import GlobalStyle from '../src/styles/globals';
+import globalStyle from '../src/styles/globals';
 
-import { Global, css, ThemeProvider } from '@emotion/react';
+import { Global } from '@emotion/react';
 import { withThemeFromJSXProvider } from '@storybook/addon-styling';
 
-/* TODO: update import for your custom theme configurations */
-// import { lightTheme, darkTheme } from '../path/to/themes';
-
-/* TODO: replace with your own global styles, or remove */
-const GlobalStyles = () => (
-  <Global
-    styles={css`
-      body {
-        font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-      }
-    `}
-  />
-);
-
+const GlobalStyles = () => <Global styles={globalStyle} />;
 const OriginalNextImage = NextImage.default;
 
 Object.defineProperty(NextImage, 'default', {
@@ -31,18 +18,10 @@ Object.defineProperty(NextImage, 'default', {
 export const decorators = [
   (Story) => (
     <>
-      <GlobalStyle />
       <Story />
     </>
-  ), // Adds global styles and theme switching support.
+  ),
   withThemeFromJSXProvider({
-    /* Uncomment for theme switching support */
-    // themes: {
-    //   light: lightTheme,
-    //   dark: darkTheme,
-    // }
-    // defaultTheme: 'light',
-    // Provider: ThemeProvider,
     GlobalStyles,
   }),
 ];
