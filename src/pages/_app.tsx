b29af4bedccdfc2afc682aa8a361c85095b52583
Kei Ota <k_ota@yumemi.co.jp>
2023-07-31 14:15:16 +0900

    eslint-plugin-import と eslint-config-next の導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
-import globalStyle from 'src/styles/globals';
 import { Global } from '@emotion/react';
+import globalStyle from 'src/styles/globals';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
