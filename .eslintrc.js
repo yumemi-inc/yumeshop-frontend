551fbdc48a82161ba8b25585d948f01f1773878e
Kei Ota <k_ota@yumemi.co.jp>
2023-07-31 14:12:16 +0900

    ignoreファイルの設定変更

--- .eslintrc.js
+++ .eslintrc.js
@@ -48,28 +48,31 @@ module.exports = {
       },
     ],
     'react/jsx-props-no-spreading': 'off',
-    'import/no-extraneous-dependencies': ['error', {
-      // devDependenciesのimportを許容
-      devDependencies: true
-    }],
-    'storybook/prefer-pascal-case': 'off'
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        // devDependenciesのimportを許容
+        devDependencies: true,
+      },
+    ],
+    'storybook/prefer-pascal-case': 'off',
   },
   overrides: [
     {
-      'files': ['*.tsx'],
-      'rules': {
+      files: ['*.tsx'],
+      rules: {
         'react/prop-types': 'off',
       },
     },
   ],
-  "settings": {
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"]
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
-    "import/resolver": {
-      "typescript": {
-        "alwaysTryTypes": true,
-      }
-    }
-  }
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
 };
