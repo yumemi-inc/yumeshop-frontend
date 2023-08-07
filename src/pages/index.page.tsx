6759f1d260b2502b9e5af086a769efe2f5549ba9
yuuumiravy <misharv.y@gmail.com>
2023-08-07 15:10:32 +0900

    feat: useAspidaSWRでAPIからshop_itemsデータを取得する

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,25 +1,38 @@
+import useAspidaSWR from '@aspida/swr';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import { apiClient } from 'src/libs/apiClient';
 
 import { Container, Main, Title, TokenTest } from 'src/styles/home';
 
-const Home: NextPage = () => (
-  <Container>
-    <Head>
-      <title>Yumeshop</title>
-      <meta name="description" content="" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const Home: NextPage = () => {
+  const { data: shopItems } = useAspidaSWR(apiClient.shop_items);
 
-    <Main>
-      <Image src="/shopping-bag.jpg" alt="買い物袋" width={600} height={600} />
+  console.log(shopItems);
 
-      <TokenTest>
-        <Title>Welcome to Yumeshop</Title>
-      </TokenTest>
-    </Main>
-  </Container>
-);
+  return (
+    <Container>
+      <Head>
+        <title>Yumeshop</title>
+        <meta name="description" content="" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Main>
+        <Image
+          src="/shopping-bag.jpg"
+          alt="買い物袋"
+          width={600}
+          height={600}
+        />
+
+        <TokenTest>
+          <Title>Welcome to Yumeshop</Title>
+        </TokenTest>
+      </Main>
+    </Container>
+  );
+};
 
 export default Home;
